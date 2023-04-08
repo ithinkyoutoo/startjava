56bc47f73b805e678b0de754c4747d964e59ee10
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-04-08 14:39:06 +0800

    fix IfElseStatementTheme.java
    - исправил 2-9 задачи

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -34,28 +34,24 @@ public class IfElseStatementTheme {
         int num2 = 37;
         if (num1 == num2) {
             System.out.println("Числа " + num1 + " и " + num2 + " равны");
-        } else if (num1 > num2){
+        } else if (num1 > num2) {
             System.out.println("Число " + num1 + " максимальное, а " + num2 + " минимальное");
         } else {
             System.out.println("Число " + num2 + " максимальное, а " + num1 + " минимальное");
         }
 
         System.out.println("\n3.Проверка числа\n");
-        String even;
-        String sign;
         num1 = 36;
         if (num1 == 0) {
             System.out.println("Ошибка! Введите число отличное от нуля.");
         } else {
-            if (num1 % 2 == 0){
+            String even = "нечетным";
+            String sign = "отрицательным";
+            if (num1 % 2 == 0) {
                 even = "четным";
-            } else {
-                even = "нечетным";
             }
             if (num1 > 0) {
                 sign = "положительным";
-            } else {
-                sign = "отрицательным";
             }
             System.out.println("Число " + num1 + " является " + even + ", " + sign);
         }
@@ -65,11 +61,11 @@ public class IfElseStatementTheme {
         num2 = 563;
         System.out.println(num1 + "\n" + num2 + "\n");
         int numHundred1 = num1 / 100;
-        int numTen1 = num1 % 100 / 10;
-        int numOne1 = num1 % 100 % 10;
+        int numTen1 = num1 / 10 % 10;
+        int numOne1 = num1 % 10;
         int numHundred2 = num2 / 100;
-        int numTen2 = num2 % 100 / 10;
-        int numOne2 = num2 % 100 % 10;
+        int numTen2 = num2 / 10 % 10;
+        int numOne2 = num2 % 10;
         if (numHundred1 != numHundred2 && numTen1 != numTen2 && numOne1 != numOne2) {
             System.out.println("Нет совпадений");
         } else {
@@ -87,11 +83,11 @@ public class IfElseStatementTheme {
 
         System.out.println("\n5.Определение символа по его коду\n");
         char ch1 = '\u0057';
-        if (ch1 > '`' && ch1 < '{' ) {
+        if (ch1 >= 'a' && ch1 <= 'z' ) {
             System.out.println(ch1 + " маленькая буква");
-        } else if (ch1 > '@' && ch1 < '[') {
+        } else if (ch1 >= 'A' && ch1 <= 'Z') {
             System.out.println(ch1 + " большая буква");
-        } else if (ch1 > '/' && ch1 < ':') {
+        } else if (ch1 >= '0' && ch1 <= '9') {
             System.out.println(ch1 + " число");
         } else {
             System.out.println("не буква и не число");
@@ -99,12 +95,10 @@ public class IfElseStatementTheme {
 
         System.out.println("\n6.Подсчет суммы вклада и начисленных банком %\n");
         int sum = 300_000;
-        int percent;
-        if (sum < 100_000) {
-            percent = sum / 100 * 5;
-        } else if (sum >= 100_000 && sum < 300_000) {
+        int percent = sum / 100 * 5;
+        if (sum >= 100_000 && sum < 300_000) {
             percent = sum / 100 * 7;
-        } else {
+        } else if (sum >= 300_000) {
             percent = sum / 10;
         }
         System.out.println("Сумма вклада: " + sum +"\n" +
@@ -112,83 +106,67 @@ public class IfElseStatementTheme {
                 "Итоговая сумма с %: " + (sum + percent));
 
         System.out.println("\n7.Определение оценки по предметам\n");
-        int historyPer = 59;
-        int programPer = 91;
-        int historyGrade;
-        if (historyPer <= 60) {
+        int historyPercent = 59;
+        int historyGrade = 5;
+        if (historyPercent <= 60) {
             historyGrade = 2;
-        } else if (historyPer > 60 && historyPer <= 73) {
+        } else if (historyPercent > 60 && historyPercent <= 73) {
             historyGrade = 3;
-        } else if (historyPer > 73 && historyPer <= 91) {
+        } else if (historyPercent > 73 && historyPercent <= 91) {
             historyGrade = 4;
-        } else {
-            historyGrade = 5;
         }
-        int programGrade;
-        if (programPer <= 60) {
+        int programPercent = 91;
+        int programGrade = 5;
+        if (programPercent <= 60) {
             programGrade = 2;
-        } else if (programPer > 60 && programPer <= 73) {
+        } else if (programPercent > 60 && programPercent <= 73) {
             programGrade = 3;
-        } else if (programPer > 73 && programPer <= 91) {
+        } else if (programPercent > 73 && programPercent <= 91) {
             programGrade = 4;
-        } else {
-            programGrade = 5;
         }
-        int avrGrade = (historyGrade + programGrade) / 2;
-        int avrPer = (historyPer + programPer) / 2;
+        int averageGrade = (historyGrade + programGrade) / 2;
+        int averagePercent = (historyPercent + programPercent) / 2;
         System.out.println("Оценки: " + historyGrade + " история" + "\n" +
                 "        " + programGrade + " программирование" + "\n" +
-                "Средняя оценка: " + avrGrade + "\n" +
-                "Средний процент: " + avrPer);
+                "Средняя оценка: " + averageGrade + "\n" +
+                "Средний процент: " + averagePercent);
 
         System.out.println("\n8.Расчет прибыли за год\n");
         int priceRent = 5_000;
         int soldGoods = 13_000;
-        int purchGoods = 9_000;
-        int annProfit = (soldGoods - purchGoods - priceRent) * 12;
-        if (annProfit > 0) {
-            System.out.println("Прибыль за год: +" + annProfit);
+        int costGoods = 9_000;
+        int annualProfit = (soldGoods - costGoods - priceRent) * 12;
+        if (annualProfit > 0) {
+            System.out.println("Прибыль за год: +" + annualProfit);
         } else {
-            System.out.println("Прибыль за год: " + annProfit);
+            System.out.println("Прибыль за год: " + annualProfit);
         }
 
         System.out.println("\n9.Подсчет количества банкнот\n");
-        int srcNum = 567;
-        numHundred1 = srcNum / 100;
-        numTen1 = srcNum % 100 / 10;
-        numOne1 = srcNum % 100 % 10;
-        if (srcNum > 1100) {
+        int srcUsd = 567;
+        int reserveHundred = 10;
+        int reserveTen = 5;
+        int reserveOne = 50;
+        int sumReserve = reserveHundred * 100 + reserveTen * 10 + reserveOne;
+        if (srcUsd > sumReserve) {
             System.out.println("Банкнот не хватает для выдачи нужной суммы");
         } else {
-            if (srcNum == 1100) {
-                numHundred1 = 10;
-                numTen1 = 5;
-                numOne1 = 50;
-            } else if (numTen1 > 5) {
-                switch (numTen1) {
-                    case 6:
-                        numTen1 = 5;
-                        numOne1 = numOne1 + 10;
-                        break;
-                    case 7:
-                        numTen1 = 5;
-                        numOne1 = numOne1 + 20;
-                        break;
-                    case 8:
-                        numTen1 = 5;
-                        numOne1 = numOne1 + 30;
-                        break;
-                    case 9:
-                        numTen1 = 5;
-                        numOne1 = numOne1 + 40;
-                        break;
+            int numHundred = srcUsd / 100;
+            int numTen = srcUsd / 10 % 10;
+            int numOne = srcUsd % 10;
+            if (srcUsd == sumReserve) {
+                numHundred = reserveHundred;
+                numTen = reserveTen;
+                numOne = reserveOne;
+            } else if (numTen > reserveTen) {
+                numOne += (numTen - reserveTen) * 10;
+                numTen = reserveTen;
                 }
-            }
-            System.out.println("К выдаче: " +"\n" +
-                numHundred1 + " по 100" + "\n" +
-                numTen1 + " по 10" + "\n" +
-                numOne1 + " по 1" +"\n" +
-                "К выдаче " + srcNum + " USD");
+            System.out.println("К выдаче: " + "\n" +
+                    numHundred + " по 100" + "\n" +
+                    numTen + " по 10" + "\n" +
+                    numOne + " по 1" + "\n" +
+                    "К выдаче " + srcUsd + " USD");
         }
     }
 }
