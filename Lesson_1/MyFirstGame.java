177a12bc25b011a3034ada65c45a2646a3f7d0d6
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-05-05 19:13:55 +0800

    create MyFirstGame.java

---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,14 @@
+public class MyFirstGame {
+
+    public static void main(String[] args) {
+        int searchNum = 7;
+        for (int i = 1; i <= 100; i++) {
+            if (i == searchNum) {
+                System.out.println("Вы победили!");
+                i = 100;
+            } else {
+                System.out.printf("число %d меньше того, что загадал компьютер %n", i);
+            }
+        }
+    }
+}
