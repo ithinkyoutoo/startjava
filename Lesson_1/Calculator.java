346fd5557b184db60be07826cc49bc9774c0fffe
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-05-05 19:10:35 +0800

    create Calculator.java

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 4;
+        char sign = '^';
+        int result = 1;
+        if (sign == '+') {
+            result = num1 + num2;
+        } else if (sign == '-') {
+            result = num1 - num2;
+        } else if (sign == '*') {
+            result = num1 * num2;
+        } else if (sign == '/') {
+            result = num1 / num2;
+        } else if (sign == '^') {
+            for (int i = num2; i > 0; i--) {
+                result *= num1;
+            }
+        } else {
+            result = num1 % num2;
+        }
+        System.out.println("\n" + num1 + " " + sign + " " + num2 + " = " + result);
+    }
+}
