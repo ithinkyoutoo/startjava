10430e73a4c79dd256012ee81e207d229028582b
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-05-08 12:01:23 +0800

    create Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    String gender = "Мужской";
+    String name = "Иван";
+    float height = 1.78f;
+    float weight = 78.4f;
+    int age = 37;
+
+    String walk() {
+        return "Сделал шаг";
+    }
+
+    boolean sit() {
+        System.out.println("Сидит");
+        return true;
+    }
+
+    String run() {
+        return "Бежит";
+    }
+
+    String speak() {
+        return "Хватит мной командовать!";
+    }
+
+    String learnJava() {
+        return "+1 к пониманию Java";
+    }
+}
