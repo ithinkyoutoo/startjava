c97b6acf27df8cdd7d284222d7b8ec7576aff450
ithinkyoutoo <ithinkyoutoo@gmail.com>
2023-06-12 13:21:48 +0800

    fix ArrayTheme.java

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -9,7 +9,7 @@ public class ArrayTheme {
         array.printProduct();
         array.delete();
         array.printReverseStairs();
-        array.generateNums();
+        array.generateUniqueNums();
         array.copyFullStrings();
     }
 
@@ -17,31 +17,31 @@ public class ArrayTheme {
         System.out.println("\n1. Реверс значений массива\n");
         int[] intArr = {5, 2, 1, 7, 3, 6, 4};
         print(intArr);
-        int tempNum;
-        int length = intArr.length - 1;
-        for (int i = 0; i < length / 2; i++) {
-            tempNum = intArr[i];
-            intArr[i] = intArr[length - i];
-            intArr[length - i] = tempNum;
+        int length = intArr.length;
+        for (int i = 0; i < length; i++) {
+            length--;
+            int tempNum = intArr[i];
+            intArr[i] = intArr[length];
+            intArr[length] = tempNum;
         }
         print(intArr);
     }
 
     private void printProduct() {
         System.out.println("\n2. Вывод произведения элементов массива\n");
-        int[] intArr = new int[10];
-        int length = intArr.length;
+        int[] multipliers = new int[10];
+        int length = multipliers.length;
         for (int i = 0; i < length; i++) {
-            intArr[i] = i;
+            multipliers[i] = i;
         }
         int productNums = 1;
         String result;
         for (int i = 1; i < length - 1; i++) {
-            productNums *= intArr[i];
-            result = intArr[i] != intArr[length - 2] ? " * " : " = " + productNums;
-            System.out.print(intArr[i] + result);
+            productNums *= multipliers[i];
+            result = i != length - 2 ? " * " : " = " + productNums;
+            System.out.print(multipliers[i] + result);
         }
-        System.out.println("\n" + intArr[0] + "  " + intArr[9]);
+        System.out.println("\n" + multipliers[0] + "  " + multipliers[9]);
     }
 
     private void delete() {
@@ -53,8 +53,9 @@ public class ArrayTheme {
         }
         print(randomDoubles);
         int countZeros = 0;
+        double middleNum = randomDoubles[length / 2];
         for (int i = 0; i < length; i++) {
-            if (randomDoubles[i] > randomDoubles[length / 2]) {
+            if (randomDoubles[i] > middleNum) {
                 randomDoubles[i] = 0;
                 countZeros++;
             }
@@ -66,7 +67,7 @@ public class ArrayTheme {
     private void print(double[] array) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == 0) {
-                System.out.printf("%8.0f", array[i]);
+                System.out.printf("%8.3f", array[i]);
             } else {
                 System.out.printf("%8.3f", array[i]);
             }
@@ -79,44 +80,42 @@ public class ArrayTheme {
 
     private void printReverseStairs() {
         System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке\n");
-        char[] charArr = new char[26];
-        int index = 0;
-        for (char i = 'A'; i <= 'Z'; i++) {
-            charArr[index] = i;
-            index++;
+        char[] capitalLetters = new char[26];
+        int length = capitalLetters.length;
+        for (char i = 0; i < length; i++) {
+            capitalLetters[i] = (char) ('A' + i);
         }
-        int length = charArr.length;
         String result = "";
         for (int i = 0; i < length; i++) {
-            result += charArr[length - (1 + i)];
+            result += capitalLetters[length - (1 + i)];
             System.out.println(result);
         }
     }
 
-    private void generateNums() {
+    private void generateUniqueNums() {
         System.out.println("\n5. Генерация уникальных чисел\n");
-        int[] intArr = new int[30];
-        int length = intArr.length;
+        int[] uniqueNums = new int[30];
+        int length = uniqueNums.length;
         for (int i = 0; i < length; i++) {
-            intArr[i] = (int) (60 + Math.random() * 40);
-            for (int j = 0; j < i; j++) {
-                if (intArr[i] == intArr[j]) {
-                    intArr[i] = (int) (60 + Math.random() * 40);
-                    j = -1;
+            int num = 0;
+            for (int j = i; j >= 0; j--) {
+                if (num == uniqueNums[j]) {
+                    num = (int) (60 + Math.random() * 40);
+                    j = i;
                 }
             }
+            uniqueNums[i] = num;
         }
-        int tempNum;
         for (int i = length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (intArr[j] > intArr[j + 1]) {
-                    tempNum = intArr[j + 1];
-                    intArr[j + 1] = intArr[j];
-                    intArr[j] = tempNum;
+                if (uniqueNums[j] > uniqueNums[j + 1]) {
+                    int tempNum = uniqueNums[j + 1];
+                    uniqueNums[j + 1] = uniqueNums[j];
+                    uniqueNums[j] = tempNum;
                 }
             }
         }
-        print(intArr);
+        print(uniqueNums);
     }
 
     private void copyFullStrings() {
@@ -129,21 +128,21 @@ public class ArrayTheme {
                 length++;
             }
         }
-        String[] copyStringArr = new String[length];
+        String[] fullStrings = new String[length];
         int index = 0;
-        int countFullString = 0;
+        int countFullStrings = 0;
         for (int i = 0; i < stringArr.length; i++) {
             if (!stringArr[i].isBlank()) {
                 index++;
-                countFullString++;
+                countFullStrings++;
             }
             if (stringArr[i].isBlank()) {
-                System.arraycopy(stringArr, i - countFullString,
-                        copyStringArr, index - countFullString, countFullString);
-                countFullString = 0;
+                System.arraycopy(stringArr, i - countFullStrings,
+                        fullStrings, index - countFullStrings, countFullStrings);
+                countFullStrings = 0;
             }
         }
-        print(copyStringArr);
+        print(fullStrings);
     }
 
     private void print(String[] array) {
